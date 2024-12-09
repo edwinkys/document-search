59ca41180735bb0f1e729df1aef3880608a577e7
Edwin Kys <itsedwinkys@gmail.com>
2024-12-09 00:03:29 -0600

    feat(web): improve routing structure

---
+++ web/src/routes/(landing)/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { flip } from "svelte/animate"
+  import { alerts } from "$lib/stores"
+  import Wordmark from "$lib/components/utils/wordmark.svelte"
+  import AlertCard from "$lib/components/cards/alert.svelte"
+</script>
+
+<ul class="alert-container space-y-1">
+  {#each $alerts as alert (alert.id)}
+    <li animate:flip={{ duration: 200 }}>
+      <AlertCard {alert} />
+    </li>
+  {/each}
+</ul>
+
+<header>
+  <div class="header-container">
+    <Wordmark />
+  </div>
+</header>
+
+<slot />
+
+<style lang="postcss">
+  .header-container {
+    @apply flex flex-row items-center justify-between;
+    @apply container mx-auto px-6;
+  }
+</style>
